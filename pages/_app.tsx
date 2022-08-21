2bddc5bcbc2bbe7c43f94f21b4eb9f799ba00809
david om <rahrahfez@hotmail.com>
2022-08-20 22:02:30 -0400

    initial commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,23 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { ChakraProvider } from '@chakra-ui/react'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function
+MyApp({ Component, pageProps }: AppProps) {
+  return (
+  <ChakraProvider>
+    <Component {...pageProps} />
+  </ChakraProvider>
+)}
+
+const quokkoTest = function (b: number) {
+  let a = 5;
+  console.log(a);
+  return a + b;
 }
+
+const test = quokkoTest(2);
+
+test
 
 export default MyApp
